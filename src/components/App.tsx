1e5717bae320afb4c63b9b2bda0b8c695e939dce
Ghena B <gbujag@gmail.com>
2023-11-07 21:33:02 +0200

    initial commit

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,8 @@
-import React, {Suspense, useContext, useState} from 'react';
+import React, {Suspense} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
 import "../styles/index.scss"
 import {MainPageAsync} from "../pages/MainPage/MainPage.async";
 import {AboutPageAsync} from "../pages/AboutPage/AboutPage.async";
-import {Theme, ThemeContext} from "../theme/ThemeContext";
 import {useTheme} from "../theme/useTheme";
 
 
