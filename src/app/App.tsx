426e83c7dc4e21316d168d40db3c745892ad5ca9
Ghena B <gbujag@gmail.com>
2023-11-16 17:12:48 +0200

    add page loader, add notfound page and error boundary

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import './styles/index.scss';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
@@ -8,6 +8,7 @@ import { Sidebar } from 'widgets/Sidebar/ui';
 
 function App() {
     const { theme } = useTheme();
+
     return (
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
