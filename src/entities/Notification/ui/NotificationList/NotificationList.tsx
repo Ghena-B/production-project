a4aedb3c034ae23958e0213210e35d9f2f016946
Ghena B <gbujag@gmail.com>
2024-02-23 22:23:49 +0200

    add popover, add notifications completed

---
+++ src/entities/Notification/ui/NotificationList/NotificationList.tsx
@@ -0,0 +1,45 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { memo } from 'react';
+import { VStack } from 'shared/ui/Stack';
+import { NotificationItem } from 'entities/Notification/ui/NotificationItem/NotificationItem';
+import { Skeleton } from 'shared/ui/Skeleton/Skeleton';
+import { useNotifications } from '../../api/notificationApi';
+import cls from './NotificationList.module.scss';
+
+interface NotificationProps {
+    className?: string;
+}
+
+export const NotificationList = memo((props: NotificationProps) => {
+    const { className } = props;
+    const { t } = useTranslation();
+    const { data, isLoading } = useNotifications(null, {
+        pollingInterval: 5000,
+    });
+    if (isLoading) {
+        return (
+            <VStack
+                gap="16"
+                max
+                className={classNames(cls.Notification, {}, [className])}
+            >
+                <Skeleton width="100%" border="8px" height="50px" />
+                <Skeleton width="100%" border="8px" height="50px" />
+                <Skeleton width="100%" border="8px" height="50px" />
+            </VStack>
+        );
+    }
+
+    return (
+        <VStack
+            gap="16"
+            max
+            className={classNames(cls.Notification, {}, [className])}
+        >
+            {data?.map((item) => (
+                <NotificationItem item={item} key={item.id} />
+            ))}
+        </VStack>
+    );
+});
