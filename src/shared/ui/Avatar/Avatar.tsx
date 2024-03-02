26cec8732a1cc97cafbfd9477960b4ef34e2c9e8
Ghena B <gbujag@gmail.com>
2024-03-02 17:01:18 +0200

    add appImage, fallback, errorFallback

--- src/shared/ui/Avatar/Avatar.tsx
+++ src/shared/ui/Avatar/Avatar.tsx
@@ -1,6 +1,11 @@
 import { CSSProperties, useMemo } from 'react';
 
+import UserIcon from '../../assets/icons/user-filled.svg';
+import { AppImage } from '../AppImage';
+import { Icon } from '../Icon';
+
 import { classNames } from '@/shared/lib/classNames/classNames';
+import { Skeleton } from '@/shared/ui/Skeleton';
 
 import cls from './Avatar.module.scss';
 
@@ -9,16 +14,23 @@ interface AvatarProps {
     src?: string,
     size?: number,
     alt?: string,
+    fallbackInverted?: boolean;
 }
 
 export const Avatar = ({
-    className, src, size, alt,
+    className,
+    src,
+    size = 100,
+    alt,
+    fallbackInverted,
 }: AvatarProps) => {
     const styles = useMemo<CSSProperties>(() => ({
-        width: size || 100,
-        height: size || 100,
+        width: size,
+        height: size,
     }), [size]);
+    const fallback = <Skeleton width={size} height={size} border="50%" />;
+    const errorFallback = <Icon inverted={fallbackInverted} width={size} height={size} Svg={UserIcon} />;
     return (
-        <img src={src} alt={alt} style={styles} className={classNames(cls.Avatar, {}, [className])} />
+        <AppImage fallback={fallback} errorFallback={errorFallback} src={src} alt={alt} style={styles} className={classNames(cls.Avatar, {}, [className])} />
     );
 };
